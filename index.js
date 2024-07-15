0b61ae769228376a08675e2acb8152798c93534d
YL <yuval.lieberman@2bprecisehealth.com>
2024-07-15 06:53:39 +0300

    enable connection reconnets

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server,  {
+  connectionStateRecovery: {}
+});
 
 const { Chess } = require('chess.js');
 
